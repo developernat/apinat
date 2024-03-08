29241fbe70991d26bd4c601543aca0cb8c43c811
NatDev <neooncillo@outlook.cl>
2024-03-08 08:50:56 -0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,30 @@
 import { Module } from '@nestjs/common';
-
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { concat } from 'rxjs';
 
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://natsudev:NFJvqQExLAaew9QG@cluster0.nxinxbp.mongodb.net/natdevDB?retryWrites=true&w=majority&appName=cluster0'), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot('mongodb+srv://'
+      + process.env.MONGO_USER
+      + ':'
+      + process.env.MONGO_PASSWORD
+      + '@'
+      + process.env.MONGO_CLUSTER
+      + '/'
+      + process.env.MONGO_DATABASE
+      + '?retryWrites=true&w=majority'
+    ),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { concat } from 'rxjs';
+
+console.log(  );
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
